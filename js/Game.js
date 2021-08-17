3f888b49f7ac3a1775d11906643fdfcea5b91b06
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-08-17 13:34:53 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,75 @@
+class Game {
+  constructor() {}
+  //BP
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  //BP
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  // TA
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+  }
+
+  //BP
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  //SA
+  play() {
+    this.handleElements();
+
+    //Player.getPlayersInfo();
+
+   /*  if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      //index of the array
+      var index = 0;
+      for (var plr in allPlayers) {
+        //use data form the database to display the cars in x and y direction
+        var x = allPlayers[plr].positionX;
+        var y = height - allPlayers[plr].positionY;
+
+        cars[index].position.x = x;
+        cars[index].position.y = y;
+
+        //add 1 to the index for every loop
+        index = index + 1;
+      }
+
+      // handling keyboard events
+      if (keyIsDown(UP_ARROW)) {
+        player.positionY += 10;
+        player.update();
+      }
+ */
+      drawSprites();
+    }
+  }
+
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+
+//BP
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+}
+
+//BP
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
+
+//BP
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
